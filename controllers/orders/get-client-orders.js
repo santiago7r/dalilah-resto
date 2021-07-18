c4fae969856d6ff73b71f4c901c8c6344d1559e3
John Doe <eddy.rhenals7@gmail.com>
2021-07-18 17:54:22 -0500

    Adding Swagger

---
+++ controllers/orders/get-client-orders.js
@@ -0,0 +1,27 @@
+
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('mysql://root:root@localhost:3306/delilah');
+
+const clientOrder = async (req, res) => {
+    const {id_usuarios}=req.body;
+    console.log(req.body);
+    try {
+        const clientOrder = await sequelize.query(
+            `SELECT id_ordenes
+            FROM delilah.ordenes
+            WHERE id_usuarios=id_usuarios ;`, {
+                type: sequelize.QueryTypes.SELECT,
+            }
+        );
+        res.status(201).json({msg: 'This are all the orders', body: clientOrder});
+        
+    } catch (error) {
+        res.status(500).json({msg: 'Error', error: { message: error.message, stack: error.stack }}); 
+    }    
+};
+
+module.exports = 
+{
+    clientOrder
+};
--- routes/orders/index.js
+++ routes/orders/index.js
@@ -4,17 +4,17 @@ const middleware = require('../../middleware/middleware');
  
 const createOrder = require('../../controllers/orders/create-orders');
 const getOrder = require('../../controllers/orders/get-orders')
-// const getAllOrders = require();
-// const getOrder = require();
+const clientOrder = require('../../controllers/orders/get-client-orders')
 const updateorders = require('../../controllers/orders/update-order');
-// const deleteOrder = require(); 
 
-// middleware.validarTokenAdmin,
+
+
 
 router.post('/order', createOrder.createOrders);
 router.get('/allOrders', middleware.validarTokenAdmin, getOrder.getOrder);
+router.get('/clientOrders', clientOrder.clientOrder);
 router.put('/order',  middleware.validarTokenAdmin, updateorders.updateorders);
-// router.delete('/order', middleware.validarTokenAdmin, deleteorder.deleteorder);
+
 
 
 module.exports = router;
